bb1455ae6db4740bce78fff5ce1e759c04423866
EsterKom <esterkmark@gmail.com>
2022-08-23 16:53:43 +0200

    carte ajouté

--- interactive map/assets/js/main.js
+++ interactive map/assets/js/main.js
@@ -18,20 +18,14 @@ fetch("./parts/footer.html")
   });
 
 
-// Animate with barba.js // --------------------------------------------------
-barba.init({
-    schema: {
-        prefix: 'data-custom',
-        wrapper: 'wrap'
-      }
-})
-
 // Leaflet.js // -------------------------------------------------------------
 let map = L.map('map').setView([51.505, -0.09], 13)
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
