bcf8eb36faa3560c89ef8f10cfdd379d47adf6eb
EsterKom <esterkmark@gmail.com>
2022-08-25 21:01:57 +0200

    merge with added place in database success

--- interactive_map/assets/js/main.js
+++ interactive_map/assets/js/main.js
@@ -54,70 +54,55 @@ function onMapClick(e) {
   }
 
   //on place le marker au clic
-  clicMarker = new L.marker(e.latlng, { draggable: true })
-    .addTo(map)
-    .bindPopup
-    // FORMLAIRE AVEC BOUTON ENVOYER POUR UN EVENTUEL AJOUT DE DONNER PAR LE USER QUI SOUHAITE AJOUTER UN LIEUX QU'IL SOUHAITE PARTAGER
-    // "<form id='myform'>" +
-    // "<input type='text' id='name' name='name' placeholder='Nom établissement'>" +
-    // "<br><input type='text' id='type' name='adress' placeholder ='adresse'>" +
-    // "<br><select name='category_id' id='categories-select'>" +
-    // "<option value='1'>Restaurants</option>"+
-    // "<option value='2'>fastfood</option>"+
-    // "<option value='3'>Pizzas</option>" +
-    // "</select>" +
-    // "<br><input name='city-id' placeholder='city'>" +
-    // "<br><input id='lon' name= 'longitude' placeholder='longitude' type='hidden' value='"+e.latlng.lat.toFixed(2)+"'>" +
-    // "<br><input id='lat' name= 'latitude' placeholder='latitude' type='hidden' value='"+e.latlng.lng.toFixed(2)+"'>" +
-    // "<button type='submit'>Envoyer</button>" +
-    // "</form>"
-
-    // FORMULAIRE POUR INDIQUER SUR LA MAP LES INFOS CONCERNANT DES LIEUX REMPLI PAR L'ADMIN DANS LA BDD
-    // QUI APPARAITRONS EN CLIQUANT SUR LE MARKER
-
-//     "<form id='myform'>" +
-//     "<input type='text' id='name' name='name' placeholder='Nom établissement'>" +
-//     "<br><input type='text' id='category' name='category' placeholder ='type de lieux'>" +
-//     "<br><input type='text' id='adress' name='adress' placeholder ='adresse'>" +
-//     "<br><input name='city-id' placeholder='ville'>" +
-//     "<br><input id='lon' name= 'longitude' placeholder='longitude' type='hidden' value='"+e.latlng.lat.toFixed(2)+"'>" +
-//     "<br><input id='lat' name= 'latitude' placeholder='latitude' type='hidden' value='"+e.latlng.lng.toFixed(2)+"'>" +
-//     "</form>"
-  );
+  clicMarker = new L.marker(e.latlng, { draggable: true }).addTo(map).bindPopup(
+    //FORMLAIRE AVEC BOUTON ENVOYER POUR UN EVENTUEL AJOUT DE DONNER PAR LE USER QUI SOUHAITE AJOUTER UN LIEUX QU'IL SOUHAITE PARTAGER
+    "<form id='myform'>" +
+    "<input type='text' id='name' name='name' placeholder='Nom établissement'>" +
+    "<br><input type='text' id='adress' name='adress' placeholder='adresse'>" +
+    // "<br><input type='text' id='category_id1' name='category_id1' placeholder='category'>" +
+    // "<br><input id='city_id' name='city_id' placeholder='city'>" +
+    // "<br><input id='longitude' name='longitude' placeholder='longitude' type='hidden' value='"+e.latlng.lat.toFixed(2)+"'>" +
+    // "<br><input id='latitude' name='latitude' placeholder='latitude' type='hidden' value='"+e.latlng.lng.toFixed(2)+"'>" +
+    "<button type='submit'>Envoyer</button>" +
+    "</form>"
+    )
  }
 
 //on le surveille au clic
 map.on("click", onMapClick);
 
-//   // traitement formulaire
 
   //   // traitement formulaire
 
-  //   document.addEventListener('submit', function (event){
-  //     event.preventDefault();
-  //     console.log("c'est passé ?", event.target);
-  //     let form = document.getElementById('myform');
-  //     console.log(form);
-  //     //on recupere tout le form a partir de l'evenenement, et on hydrate le Formdata avec
-  //     let data= new FormData(event.target);
-  //     console.log(data)
-  //     let value = Object.fromEntries(data.entries());
-  //     console.log({value})
-  //     let name = document.getElementById('name').value;
-  //     let type = document.getElementById('type').value;
+    document.addEventListener('submit', function (event){
+      event.preventDefault();
+      console.log("c'est passé ?", event.target);
+      let form = document.getElementById('myform');
+      console.log(form);
+      //on recupere tout le form a partir de l'evenenement, et on hydrate le Formdata avec
+      let data= new FormData(event.target);
+      console.log(data)
+      let value = Object.fromEntries(data.entries());
+      console.log({value})
+      let namePlace = document.getElementById('name').value;
+      let type = document.getElementById('adress').value;
+      // let category_id1 = document.getElementById('category_id1').value;
+      // let city_id = document.getElementById('city_id').value;
+      // let lng = document.getElementById('longitude').value;
+      // let lat = document.getElementById('latitude').value;
 
-  //     console.log(JSON.stringify(value));
+      console.log(JSON.stringify(value));
 
-  //     fetch('../serveur/insertData.php', {
-  //         method: 'POST',
-  //         body: JSON.stringify(value),
-  //         headers: {
-  //             "Content-Type": "application/json; charset=UTF-8"
-  //         }
-  //     })
-  //     .then((response)=> console.log(response.json()))
-  //     .then((data)=> console.log(data))
-  // });
+      fetch('../serveur/insertData.php', {
+          method: 'POST',
+          body: JSON.stringify(value),
+          headers: {
+              "Content-Type": "application/json; charset=UTF-8"
+          }
+      })
+      .then((response)=> console.log(response.json()))
+      .then((data)=> console.log(data))
+  });
 
 //liste des marqueurs
       fetch('../serveur/list.php') // on fait un fetch pour faire appel à lapi 
--- serveur/insertData.php
+++ serveur/insertData.php
@@ -3,43 +3,51 @@ include 'bdd.php';
 
 
 header('Content-Type: application/json');
-$content = json_decode(stripslashes(file_get_contents("php://input")),true);
+$content = json_decode(stripslashes(file_get_contents("php://input")), true);
 
 
 // get the place details concerne le formulaire pour l'ajout de lieux par le user s'il se connecte
 // mais ne marche pas encore à revoir!!
 
-$idPlace = $content['id'];
-$namePlace= $content['name'];
-$adressPlace = $content['adress'];
-$idCategory = $content['category_id1'];
-$idCity = $content['city_id'];
-$longitude = $content['longitude'];
-$latitude = $content['latitude'];
-    
+if(isset($content['submit'])){
 
+    $name= $content['name'];
+    $adress = $content['adress'];
+    // $idCategory2 = $content['category_id1'];
+    // $idCategory = (int)$idCategory2;
+    // $idCity2 = $content['city_id'];
+    // $idCity = (int)$idCity2;
+    // $longitude2 = $content['longitude'];
+    // $longitude = $longitude2;
+    // $latitude2 = $content['latitude'];
+    // $latitude = $latitude2;
+    
 //insert into mysql table
 
-$sql = "INSERT INTO `place`(`id`, `name`, `adress`, `lat`, `lng`, `city_id`, `category_id1`) VALUES
-('$idPlace','$namePlace','$adressPlace','$latitude','$longitude','$idCity','$idCategory')";
-if(!mysql_query($sql,$conn))
 
-{
-    die('Error : ' . mysql_error());
+//$sql = "Insert into place2 (name, adress, lat, lng, city_id, category_id1) values (?,?,?,?,?,?);";
+$sql = "Insert into place2 (name, adress) values ($name, $adress)";
+//$stmt = $conn->stmt_init();
+if (!$stmt->prepare($sql)) {
+    echo "something went wrong!!!";
+    exit();
 }
+//$stmt->bind_param('ss', $name, $adress);
+$stmt->execute();
+if ($stmt->affected_rows) {
+    http_response_code(200);
+    echo "Congratulation!!\n Registration successful\n";
+}
+exit();
 
-?>
+// $sql = "INSERT INTO place (name, adress, lat, lng, city_id, category_id1) VALUES
+// ('$name','$adress', '$latitude','$longitude','$idCity','$idCategory')";
+// // if(!mysql_query($sql,$conn))
 
-
-
-
-
-
-
-
-
-
-
+// {
+//     die('Error : ' . mysql_error());
+// }
+}
 // traitement/
-    echo( json_encode($content));
+    echo( json_encode(['status_code'=>200]));
 ?>
